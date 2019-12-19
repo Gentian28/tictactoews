b2579f4ce8a628901dd157fd1f780a847f18710c
Gentian Shkembi <gentian.shkembi@gmail.com>
2019-12-19 18:02:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+//Port from environment variable or default - 4001
+const port = process.env.PORT || 4001;
+
+//Setting up express and adding socketIo middleware
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const connections = [];
+
+io.sockets.on('connection', (socket) => {
+    connections.push(socket);
+    console.log(' %s sockets is connected', connections.length);
+
+    socket.on('disconnect', () => {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log(' %s sockets is connected', connections.length);
+    });
+
+    socket.on('sending message', (message) => {
+        console.log('Message is received :', message);
+
+        io.sockets.emit('new message', { message: message });
+    });
+
+    socket.on('move', (index) => {
+        console.log('index is received :', index);
+
+        io.sockets.emit('move made', { index: index });
+    });
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
