13455bbf79d4be49dd7c4a4f89f46c8c6ca2596c
Gentian Shkembi <gentian.shkembi@gmail.com>
2019-12-24 17:47:24 +0100

    Rooms events

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 const connections = [];
+const games = [];
 
 app.get('/', (req, res, next) => {
     res.send('Tic-Tac-Toe Webservice');
@@ -20,25 +21,39 @@ io.sockets.on('connection', (socket) => {
     connections.push(socket);
 
     io.sockets.emit('connected players', connections.length);
+    io.sockets.emit('games', games);
 
     console.log(' %s sockets is connected', connections.length);
 
     socket.on('disconnect', () => {
+        console.log(socket.id)
+        const index = games.map(e => e.socketId).indexOf(socket.id);
+        if (index > -1) {
+            games.splice(games.indexOf(index), 1);
+        }
         connections.splice(connections.indexOf(socket), 1);
         io.sockets.emit('player disconnected', connections.length);
+        io.sockets.emit('games', games);
         console.log(' %s sockets is connected', connections.length);
     });
 
-    socket.on('sending message', (message) => {
-        console.log('Message is received :', message);
-
-        io.sockets.emit('new message', { message: message });
+    socket.on('generate game', (uuid) => {
+        console.log('generate game', { user: uuid });
+        games.push({ socketId: socket.id, userId: uuid });
+        io.sockets.emit('games', games);
     });
 
-    socket.on('move', (index) => {
-        console.log('index is received :', index);
+    // socket.on('sending message', (message) => {
+    //     console.log('Message is received :', message);
 
-        io.sockets.emit('move made', { index: index });
+    //     io.sockets.emit('new message', { message: message });
+    // });
+
+    socket.on('move', (data) => {
+        console.log('index is received :', data.index);
+        console.log('gameId :', data.gameId);
+
+        io.sockets.emit('move made', data);
     });
 
     socket.on('reset', () => {
